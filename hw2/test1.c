2ad9f3a50e6f36d83c79454c8d34c5dbe68ecbfd
MrPacMan1 <sharon.mishayev@gmail.com>
2017-11-27 14:26:16 +0200

    Revert "Merge branch 'master' of https://github.com/yuvadius/Matam"
    This reverts commit 5e0090d2a6d7d10001ed83f9a131bf988263a196, reversing
    changes made to 3c17c6c58b187e00ae44af81f8d0be53579c8f77.

--- hw2/test1.c
+++ hw2/test1.c
@@ -4,8 +4,8 @@
 #include "CourseSystem.h"
 
 #define ASSERT(test_number, test_condition)  \
-   if (!(test_condition)) {printf("\nTEST %s FAILED", test_number); } \
-   else printf("\nTEST %s OK", test_number);
+   if (!(test_condition)) {printf("\nTEST %s FAILED\n", test_number); } \
+   else printf("\nTEST %s OK\n", test_number);
 
 
 int main(int argc, char **argv)
@@ -16,45 +16,53 @@ int main(int argc, char **argv)
     CourseResult rc = COURSE_OK;
 
     sys=createSystem("Technion_2017_2018");
-    ASSERT("1.1" , sys!=NULL)
-
+    displaySystem(sys);
+    ASSERT("1.1" , sys!=NULL);
     Course c1;
     rc = createCourse("234114", "Introduction to Computer Science", -4, &c1);
-    ASSERT("1.2" , rc==COURSE_ILLEGAL_PARAMETER)
+    displaySystem(sys);
+    ASSERT("1.2" , rc==COURSE_ILLEGAL_PARAMETER);
     rc = createCourse("234114", "Introduction to Computer Science", 4, &c1);
-    ASSERT("1.3" , rc==COURSE_OK)
-
+    displaySystem(sys);
+    ASSERT("1.3" , rc==COURSE_OK);
     int n=sysNumCourses(sys);
-    ASSERT("1.4" , n==0)
+    displaySystem(sys);
+    ASSERT("1.4" , n==0);
 
     rs=sysAddCourse(sys, c1);
     n=sysNumCourses(sys);
-    ASSERT("1.5" , n==1)
+    displaySystem(sys);
+    ASSERT("1.5" , n==1);
 
     rs=sysAddCourse(sys, c1);
-    ASSERT("1.6" , rs==SYS_COURSE_ALREADY_EXIST)
+    displaySystem(sys);
+    ASSERT("1.6" , rs==SYS_COURSE_ALREADY_EXIST);
 
     rc = createCourse("234218", "Data Structures 1", 3, &c1);
     rs=sysAddCourse(sys, c1);
     n=sysNumCourses(sys);
-    ASSERT("1.5" , n==2)
+    displaySystem(sys);
+    ASSERT("1.5" , n==2);
 
     int ans=-1;
     rs=sysIsPreCourse(sys, "234122", "234114", &ans);
-    ASSERT("1.6" , rs==SYS_NOT_IN_SYSTEM && ans==-1)
+    displaySystem(sys);
+    ASSERT("1.6" , rs==SYS_NOT_IN_SYSTEM && ans==-1);
 
     rc = createCourse("234122", "Introduction to Systems Programming", 3, &c1);
     rs=sysAddCourse(sys, c1);
     n=sysNumCourses(sys);
-    ASSERT("1.7" , n==3)
-
-    rs=sysAddPreCourse(sys, "234122", "234114");
-    ASSERT("1.6" , rs==SYS_OK)
-
-    rs=sysAddPreCourse(sys, "234122", "234114");
-    ASSERT("1.8" , rs==SYS_ALREADY_PRE_COURSE)
-
     displaySystem(sys);
+    ASSERT("1.7" , n==3);
+
+    rs=sysAddPreCourse(sys, "234122", "234114");
+    displaySystem(sys);
+    ASSERT("1.6" , rs==SYS_OK);
+
+    rs=sysAddPreCourse(sys, "234122", "234114");
+    displaySystem(sys);
+    ASSERT("1.8" , rs==SYS_ALREADY_PRE_COURSE);
+
 /*
 Technion_2017_2018
 234114 Introduction to Computer Science 4
@@ -63,12 +71,14 @@ Technion_2017_2018
 */
 
     rs=sysRemovePreCourse(sys, "234122", "234114");
-    ASSERT("1.9" , rs==SYS_OK)
+    displaySystem(sys);
+    ASSERT("1.9" , rs==SYS_OK);
 
     rs=sysRemovePreCourse(sys, "234122", "234114");
-    ASSERT("1.10" , rs==SYS_NOT_PRE_COURSE)
-
     displaySystem(sys);
+    ASSERT("1.10" , rs==SYS_NOT_PRE_COURSE);
+
+
 /*
 Technion_2017_2018
 234114 Introduction to Computer Science 4
@@ -77,8 +87,9 @@ Technion_2017_2018
 */
 
     rs=sysAddPreCourse(sys, "234218", "234122");
-    ASSERT("1.11" , rs==SYS_OK)
     displaySystem(sys);
+    ASSERT("1.11" , rs==SYS_OK);
+
 /*
 Technion_2017_2018
 234114 Introduction to Computer Science 4
@@ -87,11 +98,12 @@ Technion_2017_2018
 */
 
     rs=sysRemoveCourse(sys, "234118");
-    ASSERT("1.12" , rs==SYS_NOT_IN_SYSTEM)
+    displaySystem(sys);
+    ASSERT("1.12" , rs==SYS_NOT_IN_SYSTEM);
 
     rs=sysRemoveCourse(sys, "234122");
-    ASSERT("1.13" , rs==SYS_OK)
     displaySystem(sys);
+    ASSERT("1.13" , rs==SYS_OK);
 /*
 Technion_2017_2018
 234114 Introduction to Computer Science 4
@@ -100,15 +112,17 @@ Technion_2017_2018
 /* remark - also destroy 234122 */
 
     n=sysNumCourses(sys);
-    ASSERT("1.14" , n==2)
+    displaySystem(sys);
+    ASSERT("1.14" , n==2);
 
     rs=sysUpdateCourseName(sys, "234122", "Systems Programming");
-    ASSERT("1.15" , rs==SYS_NOT_IN_SYSTEM)
+    displaySystem(sys);
+    ASSERT("1.15" , rs==SYS_NOT_IN_SYSTEM);
 
     rs=sysUpdateCourseName(sys, "234114", "Introduction to Computer Science M");
-    ASSERT("1.16" , rs==SYS_OK)
-
     displaySystem(sys);
+    ASSERT("1.16" , rs==SYS_OK);
+
 /*
 Technion_2017_2018
 234114 Introduction to Computer Science M 4
@@ -118,3 +132,4 @@ Technion_2017_2018
     destroySystem(sys);
     return 0;
 }
+
