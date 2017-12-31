b6d171bcf8be718246bb670cb9cf6bb49d9f5585
Yuval Glizerin <yuvalgliz@gmail.com>
2017-12-31 13:52:33 +0200

    no **

--- hw3_new/main.c
+++ hw3_new/main.c
@@ -11,16 +11,16 @@
 *
 * @param input_file - the input file that will be used across the program
 * @param output_file - the output file that will be used across the program
-* @param argc - the amount of parameters received from the command lin
+* @param argc - the amount of parameters received from the command line
 * @param argv - an array of oarameters received in the command line
 * @return true if no error occured and false if invalid command line parameters
 * were received(in this case we should stop the execution of the program)
 */
-bool setInputOutputFiles(File** input_file, File** output_file, int argc,
+bool setInputOutputFiles(FILE** input_file, FILE** output_file, int argc,
 						 char *argv[]) {
 	*input_file = stdin; //default as standard input file
 	*output_file = stdout; //default as standard output file
-	//not a valid amount of paramaters
+	//not a valid amount of parameters
 	if(argc != 1 && argc != 3 && argc != 5) {
 		mtmPrintErrorMessage(stdout, MTM_INVALID_COMMAND_LINE_PARAMETERS);
 		return false;
@@ -49,9 +49,10 @@ bool setInputOutputFiles(File** input_file, File** output_file, int argc,
 int main(int argc, char *argv[])
 {
 	//the input and output file that will be used in the program
-	FILE* input_file, output_file;
+	FILE* input_file = NULL;
+	FILE* output_file = NULL;
 	//if the command line parameters are invalid
-	if(setInputOutputFiles(&input_file, &output_file) == false) {
+	if(setInputOutputFiles(&input_file, &output_file, argc, argv) == false) {
 		return 0;
 	}
     return 0;
