989589fc431da1bba917eccb08e8f6f58fc2a5b4
Yuval Glizerin <yuvalgliz@gmail.com>
2017-12-31 13:26:31 +0200

    progress

--- hw3_new/main.c
+++ hw3_new/main.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
-#include "set.h"
 #include "mtm_ex3.h"
 
 int main(int argc, char *argv[])
 {
+	//not a valid amount of paramaters
     if(argc != 1 && argc != 3 && argc != 5) {
     	mtmPrintErrorMessage(stdout, MTM_INVALID_COMMAND_LINE_PARAMETERS);
     }
-    for(int i = 1; i < argc; ++i) {
+    if(argc >= 3 && strcmp(argc[1], "-i") != 0) {
+    	mtmPrintErrorMessage(stdout, MTM_INVALID_COMMAND_LINE_PARAMETERS);
+    }
+    if(argc == 5) {
+
+    }
+    for(int i = 0; i < argc; ++i) {
         printf("%s", argv[i]);
     }
     return 0;
